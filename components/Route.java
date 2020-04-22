3ca00d769960bfc707f476a4a056f17ce221b2b0
yarinab1 <57402773+yarinab1@users.noreply.github.com>
2020-04-22 15:26:26 +0300

    Update Route.java

--- components/Route.java
+++ components/Route.java
@@ -8,7 +8,8 @@ public class Route{
     private ArrayList<Road> roads = new ArrayList<>();;
     private double delay;
     private VehicleType vehicleType;
-
+    
+    //Route constructor
     public Route(ArrayList<Junction> juncs,ArrayList<Road> roads, VehicleType vehType){
         vehicleType = vehType;
         junctions.addAll(juncs);
@@ -53,22 +54,14 @@ public class Route{
 
     public Route(Junction start, Junction end, VehicleType vehType){}; // not implemented in this task.
     
+   //Sets And Gets to Route
     public Junction getStart(){return junctions.get(0);};
     public Junction getEnd(){return junctions.get(junctions.size()-1);};
     public double getDelay() { return delay; }
     public ArrayList<Junction> getJunctions() { return junctions;}
     public ArrayList<Road> getRoads() { return roads; }
 
-    public void calcDelay()
-     /*(1) set length to be a sum of delay values of all the junctions
-    on the route (2) and the time that will take this type of vehicle to pass all
-    the roads. (3) Time is calculated by dividing the distance by min(average
-    speed, maxSpeed).The delay time on junctions is calculated according
-    to worse case: (4) if there is a traffic lights on the junction, we use it’s
-    delay value multiplied by (number of entering roads minus one). (5) If
-    there is no traffic lights on the junction, the delay time is the priority
-    level of the road that leads us to this junction (the index of this road in
-    the list of roads)*/
+    public void calcDelay()//set length to be a sum of delay values of all the junctions on the route
     {
         double length=0; //1
         for(Junction item: junctions) 
